34509befd66c3dbba1dce92aca89961f2dfddc11
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-12-07 15:03:24 +0530

    first commit

--- 8.py
+++ 8.py
@@ -1,39 +1,21 @@
-def add(x,y):
-    return x+y
-
-def subtract(x,y):
-    return x-y
-
-def multiply(x,y):
-    return x*y
-
-
+def add(x,y): return x+y
+def subtract(x,y):return x-y
+def multiply(x,y):return x*y
 def divide(x,y):
     if y==0:
         return "Error! Division by zero"
     return x/y
 
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+print("Select operation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide")
 
 choice = input("Enter choice 1/2/3/4: ")
 num1 = float(input("Enter number 1 : "))
 num2 = float(input("Enter number 2 : "))
 
 
+operations = {'1':add , '2':subtract, '3':multiply,'4':divide}
 
-if(choice =='1'):
-    print("Sum is : ",add(num1,num2))
-    
-elif(choice =='2'):
-    print("subtract is : ",subtract(num1,num2))
-elif(choice =='3'):
-    print("multiply is : ",multiply(num1,num2))
-elif(choice =='4'):
-    print("divide is : ",divide(num1,num2))
+if choice in operations:
+    print("Result is : ",operations[choice](num1,num2))
 else:
     print("INVALID INPUT")
